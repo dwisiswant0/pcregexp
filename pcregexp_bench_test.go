a0bcfa1a9863eb2a85e5eca56a673b43e2ed9e38
Dwi Siswanto <git@dw1.io>
2025-05-16 17:34:09 +0700

    test: reset timer before subbench run
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- pcregexp_bench_test.go
+++ pcregexp_bench_test.go
@@ -19,6 +19,7 @@ func BenchmarkCompile(b *testing.B) {
 	}
 
 	for _, pattern := range patterns {
+		b.ResetTimer()
 		b.Run("pcregexp/"+pattern, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				re, _ := pcregexp.Compile(pattern)
@@ -26,6 +27,7 @@ func BenchmarkCompile(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/"+pattern, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				regexp.Compile(pattern)
@@ -51,12 +53,14 @@ func BenchmarkMatchString(b *testing.B) {
 		defer pcre.Close()
 		re, _ := regexp.Compile(tt.pattern)
 
+		b.ResetTimer()
 		b.Run("pcregexp/"+tt.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				pcre.MatchString(tt.text)
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/"+tt.name, func(b *testing.B) {
 			if pcregexp.NeedsPCRE(tt.pattern) {
 				b.Skip("skipping test for pattern requiring PCRE")
@@ -87,6 +91,7 @@ func BenchmarkFind(b *testing.B) {
 		defer pcre.Close()
 		re, _ := regexp.Compile(tt.pattern)
 
+		b.ResetTimer()
 		b.Run("pcregexp/Find/"+tt.name, func(b *testing.B) {
 			data := []byte(tt.text)
 			for i := 0; i < b.N; i++ {
@@ -94,6 +99,7 @@ func BenchmarkFind(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/Find/"+tt.name, func(b *testing.B) {
 			if pcregexp.NeedsPCRE(tt.pattern) {
 				b.Skip("skipping test for pattern requiring PCRE")
@@ -105,12 +111,14 @@ func BenchmarkFind(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("pcregexp/FindString/"+tt.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				pcre.FindString(tt.text)
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/FindString/"+tt.name, func(b *testing.B) {
 			if pcregexp.NeedsPCRE(tt.pattern) {
 				b.Skip("skipping test for pattern requiring PCRE")
@@ -140,6 +148,7 @@ func BenchmarkReplace(b *testing.B) {
 		defer pcre.Close()
 		re := regexp.MustCompile(tt.pattern)
 
+		b.ResetTimer()
 		b.Run("pcregexp/ReplaceAll/"+tt.name, func(b *testing.B) {
 			src := []byte(tt.text)
 			repl := []byte(tt.repl)
@@ -148,6 +157,7 @@ func BenchmarkReplace(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/ReplaceAll/"+tt.name, func(b *testing.B) {
 			src := []byte(tt.text)
 			repl := []byte(tt.repl)
@@ -156,12 +166,14 @@ func BenchmarkReplace(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("pcregexp/ReplaceAllString/"+tt.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				pcre.ReplaceAllString(tt.text, tt.repl)
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/ReplaceAllString/"+tt.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				re.ReplaceAllString(tt.text, tt.repl)
@@ -187,6 +199,7 @@ func BenchmarkFindAll(b *testing.B) {
 		defer pcre.Close()
 		re, _ := regexp.Compile(tt.pattern)
 
+		b.ResetTimer()
 		b.Run("pcregexp/FindAll/"+tt.name, func(b *testing.B) {
 			data := []byte(tt.text)
 			for i := 0; i < b.N; i++ {
@@ -194,6 +207,7 @@ func BenchmarkFindAll(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/FindAll/"+tt.name, func(b *testing.B) {
 			if pcregexp.NeedsPCRE(tt.pattern) {
 				b.Skip("skipping test for pattern requiring PCRE")
@@ -205,6 +219,7 @@ func BenchmarkFindAll(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("pcregexp/FindAllIndex/"+tt.name, func(b *testing.B) {
 			data := []byte(tt.text)
 			for i := 0; i < b.N; i++ {
@@ -212,6 +227,7 @@ func BenchmarkFindAll(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/FindAllIndex/"+tt.name, func(b *testing.B) {
 			if pcregexp.NeedsPCRE(tt.pattern) {
 				b.Skip("skipping test for pattern requiring PCRE")
@@ -242,6 +258,7 @@ func BenchmarkFindAllSubmatch(b *testing.B) {
 		defer pcre.Close()
 		re, _ := regexp.Compile(tt.pattern)
 
+		b.ResetTimer()
 		b.Run("pcregexp/FindAllSubmatch/"+tt.name, func(b *testing.B) {
 			data := []byte(tt.text)
 			for i := 0; i < b.N; i++ {
@@ -249,6 +266,7 @@ func BenchmarkFindAllSubmatch(b *testing.B) {
 			}
 		})
 
+		b.ResetTimer()
 		b.Run("stdlib/FindAllSubmatch/"+tt.name, func(b *testing.B) {
 			if pcregexp.NeedsPCRE(tt.pattern) {
 				b.Skip("skipping test for pattern requiring PCRE")
@@ -274,12 +292,14 @@ func BenchmarkExpand(b *testing.B) {
 	pcreMatch := pcre.FindStringSubmatchIndex(text)
 	reMatch := re.FindStringSubmatchIndex(text)
 
+	b.ResetTimer()
 	b.Run("pcregexp/ExpandString", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			pcre.ExpandString(nil, template, text, pcreMatch)
 		}
 	})
 
+	b.ResetTimer()
 	b.Run("stdlib/ExpandString", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			re.ExpandString(nil, template, text, reMatch)
@@ -293,12 +313,14 @@ func BenchmarkMarshal(b *testing.B) {
 	defer pcre.Close()
 	re := regexp.MustCompile(pattern)
 
+	b.ResetTimer()
 	b.Run("pcregexp/Marshal", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			pcre.MarshalText()
 		}
 	})
 
+	b.ResetTimer()
 	b.Run("stdlib/Marshal", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			re.MarshalText()
@@ -309,6 +331,7 @@ func BenchmarkMarshal(b *testing.B) {
 func BenchmarkUnmarshal(b *testing.B) {
 	pattern := []byte(`p([a-z]+)ch`)
 
+	b.ResetTimer()
 	b.Run("pcregexp/Unmarshal", func(b *testing.B) {
 		var re pcregexp.PCREgexp
 		for i := 0; i < b.N; i++ {
@@ -316,6 +339,7 @@ func BenchmarkUnmarshal(b *testing.B) {
 		}
 	})
 
+	b.ResetTimer()
 	b.Run("stdlib/Unmarshal", func(b *testing.B) {
 		var re regexp.Regexp
 		for i := 0; i < b.N; i++ {
@@ -332,6 +356,7 @@ func BenchmarkRuneReader(b *testing.B) {
 	defer pcre.Close()
 	re := regexp.MustCompile(pattern)
 
+	b.ResetTimer()
 	b.Run("pcregexp/MatchReader", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			reader := strings.NewReader(text)
@@ -339,6 +364,7 @@ func BenchmarkRuneReader(b *testing.B) {
 		}
 	})
 
+	b.ResetTimer()
 	b.Run("stdlib/MatchReader", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			reader := strings.NewReader(text)
@@ -346,6 +372,7 @@ func BenchmarkRuneReader(b *testing.B) {
 		}
 	})
 
+	b.ResetTimer()
 	b.Run("pcregexp/FindReaderIndex", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			reader := strings.NewReader(text)
@@ -353,6 +380,7 @@ func BenchmarkRuneReader(b *testing.B) {
 		}
 	})
 
+	b.ResetTimer()
 	b.Run("stdlib/FindReaderIndex", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			reader := strings.NewReader(text)
